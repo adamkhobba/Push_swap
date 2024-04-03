aff314ec7e9da451d86ae82e1bbbc2e86e2e5c74
Adam Khobba <akhobba@e1r12p2.1337.ma>
2024-04-03 08:45:19 +0000

    creating stach

--- src/push_swap.c
+++ src/push_swap.c
@@ -5,7 +5,8 @@ int main(int ac, char **av) {
 
   if (ac < 2)
     return (1);
-  top = create_stack_a(ac, av);
+  // top = create_stack_a(ac, av);
+  top = ft_create_node(ft_atoi(av[ac]));
   // while (top) {
   // printf("%d", top->data);
   // top = top->next;
