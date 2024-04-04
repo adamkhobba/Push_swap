b4894ecc6fd14f67e2042c24471e161a9bc8165c
Adam Khobba <akhobba@e1r12p5.1337.ma>
2024-04-04 16:53:00 +0000

    move :push a and b

--- src/push.c
+++ src/push.c
@@ -1,14 +1,13 @@
 #include "../include/push_swap.h"
 
-int	ft_push(t_list **from, t_list **to)
-{
-	t_list	*tmp;
+int ft_push(t_list **from, t_list **to) {
+  t_list *tmp;
 
-	if (!*from)
-		return (0);
-	tmp = (*from);
-	(*from) = (*from)->next;
-	push_stack(to, tmp->data);
-	free(tmp);
-	return (1);
+  if (!*from)
+    return (0);
+  tmp = (*from);
+  (*from) = (*from)->next;
+  push_stack(to, tmp->data);
+  free(tmp);
+  return (1);
 }
